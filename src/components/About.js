d0f3d0c783ec0d003914cae50dc1d83bd5f2c480
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-06-30 14:00:03 -0400

    adding updated file

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links.js"
 
-function About() {
+function About({bio, links }) {
+
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      { bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links 
+      github={links.github}
+      linkedin={links.linkedin}
+      />
     </div>
   );
 }
